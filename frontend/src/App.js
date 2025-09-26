e895bf6380919187561d115c1996609cd4732160
Mahesh yarramalla <135298995+Mahesh-yarramalla@users.noreply.github.com>
2025-09-26 13:17:23 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 function App() {
   const [message, setMessage] = useState("");
   useEffect(() => {
-    fetch("http://<EC2_PUBLIC_IP>:5000/api")
+    fetch("http://18.233.99.46:5000/api")
       .then(res => res.json())
       .then(data => setMessage(data.message));
   }, []);
